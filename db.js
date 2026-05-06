73471f288aea6be568dba6ba7ce8a60884859260
RemindTC <nexebike@gmail.com>
2026-05-05 21:54:30 -0700

    Update db.js for Railway

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'auto_reminder',
-  password: 'newpassword123',
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'auto_reminder',
+        password: 'newpassword123',
+        port: 5432,
+      }
+);
 
 module.exports = pool;
